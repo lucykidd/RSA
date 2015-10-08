9cc7312105b1bc156d71fd3d947d566e119492f2
lucykidd <lucy.kid@hotmail.com>
2015-10-08 16:30:00 +1100

    everything in the catkin src file

---
+++ camera/build/catkin_generated/package.cmake
@@ -0,0 +1,7 @@
+set(_CATKIN_CURRENT_PACKAGE "camera")
+set(camera_MAINTAINER "pi <pi@todo.todo>")
+set(camera_DEPRECATED "")
+set(camera_VERSION "0.0.0")
+set(camera_BUILD_DEPENDS "roscpp" "rospy" "sensor_msgs" "std_msgs")
+set(camera_RUN_DEPENDS "roscpp" "rospy" "sensor_msgs" "std_msgs")
+set(camera_BUILDTOOL_DEPENDS "catkin")
---
+++ camera/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/osboxes/catkin_ws/src/rsa/camera/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/osboxes/catkin_ws/src/rsa/camera/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/osboxes/catkin_ws/src/rsa/camera/build/devel/lib:/home/osboxes/catkin_ws/src/rsa/camera/build/devel/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/osboxes/catkin_ws/src/rsa/camera/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/osboxes/catkin_ws/src/rsa/camera/build/devel/lib/pkgconfig:/home/osboxes/catkin_ws/src/rsa/camera/build/devel/lib/i386-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/osboxes/catkin_ws/src/rsa/camera/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/osboxes/catkin_ws/src/rsa/camera/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/osboxes/catkin_ws/src/rsa/camera:$ROS_PACKAGE_PATH"
