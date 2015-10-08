b54c06f07c2d6afd14d6c3390fc9adf8c22906b7
lucykidd <z3418740@zmail.unsw.edu.au>
2015-10-08 07:16:57 +0000

    The start of an image publisher (currently just saves to a file)

--- camera/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/osboxes/catkin_ws/src/rsa/camera
-# Build directory: /home/osboxes/catkin_ws/src/rsa/camera/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
